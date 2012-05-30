a18c848f080395e18e0af6b36040ea5c07ad5dbf
Sean Clark Hess <seanhess@gmail.com>
2012-05-30 17:51:34 -0600

    upstart step

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var express = require('express')
 var app = express.createServer()
+var users = require('./users')
 
 app.get('/', function(req, res) {
     console.log("Hello World!")
     res.send("Hello World!")
+})
+
+// json/ajax call to get a user's email
+app.get('/users/:username/email', function(req, res) {
+    var username = req.param('username')
+    users.getUser(username, function(err, user) {
+        if (err) return res.send({error:err.message}, 500)
+        res.send({email:user.email})
+    })
 })
 
 var PORT = process.env.PORT || 3333
